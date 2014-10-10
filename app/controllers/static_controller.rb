32adeff15df7d9707bcc78b0d0443fa71c248886
Vikas S Yaligar <vikasyaligar.it@gmail.com>
2014-10-10 19:24:22 +0530

    Added FAQ page and updated time

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,11 +1,13 @@
 class StaticController < ApplicationController
-    before_action :authenticate_user!, except: [:root,:newbie]
 
     def root
-        render 'home'
+      render 'home'
     end
 
     def newbie
     end
 
+    def faq
+    	render 'faq'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "static#root"
   get '/newbie' => 'static#newbie'
+  get '/faq' => 'static#faq'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
