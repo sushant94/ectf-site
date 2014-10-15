0b7755bb2e3cc1f399e1968667032e4f5226bea7
Sushant <sushant.dinesh94@gmail.com>
2014-10-15 14:53:54 +0530

    Added IBM as sponsors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,19 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 protected
 
-    def authenticate_user!
-        redirect_to :new_user_registration unless user_signed_in?
-    end
+def authenticate_user!
+    redirect_to :new_user_session unless user_signed_in?
+end
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) << :teamName
-        devise_parameter_sanitizer.for(:sign_up) << :newTeam
-        devise_parameter_sanitizer.for(:sign_in) << :teamName
-        devise_parameter_sanitizer.for(:sign_up) << :organization
-    end
+def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :teamName
+    devise_parameter_sanitizer.for(:sign_up) << :newTeam
+    devise_parameter_sanitizer.for(:sign_in) << :teamName
+    devise_parameter_sanitizer.for(:sign_up) << :organization
+end
+
+def after_sign_in_path_for(resource)
+    '/dashboard'
+end
 
 end
